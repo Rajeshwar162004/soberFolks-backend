ba0b51a5d363c3f6a27feb4fd458c1e41f91bcf9
Rajeshwar162004 <rajeshvchintawarofficial16@gmail.com>
2025-11-14 23:16:38 +0530

    Update db.js
    new db connection

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOST || 'crossover.proxy.rlwy.net',
+  host: process.env.DB_HOST || 'mysql.railway.internal',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'BQUAksWCdcCleLkTTXabJUfRKvPNIKZi',
+  password: process.env.DB_PASSWORD || 'qFfhaWJmjTBecBEhligfhUcfEMdYZhQl',
   database: process.env.DB_NAME || 'SoberFolks',
-  port: process.env.DB_PORT || 52505,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
