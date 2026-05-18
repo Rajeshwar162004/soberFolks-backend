40937f03793e17bc5afbac8b75a5917060a07c90
Sidchav5 <csiddhesh768@gmail.com>
2026-05-18 13:27:09 +0530

    feat: allow WEB_GOOGLE_MAPS_API_KEY from env

--- config/constants.js
+++ config/constants.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const JWT_SECRET = process.env.JWT_SECRET || "3cd55083223f2738ec3b05d633a6c3e5559d153c6aabf1eab3438e2ece9188adc5bb5701b468f51c08e95c8b1a2522154b5863d0f3e7e5f8d444e84fb3e873bf";
 
 // Google Maps API Configuration
-const GOOGLE_MAPS_API_KEY = process.env.WEB_GOOGLE_MAPS_API_KEY || "AIzaSyDXZWx0j9N1BdFzQ0lP3bVF8SQJlP0xUhQ";
+const GOOGLE_MAPS_API_KEY = process.env.WEB_GOOGLE_MAPS_API_KEY || process.env.GOOGLE_MAPS_API_KEY || "AIzaSyDXZWx0j9N1BdFzQ0lP3bVF8SQJlP0xUhQ";
 
 // Booking System Configuration
 const RIDE_REQUEST_TIMEOUT = 120000; // 2 minutes per driver
